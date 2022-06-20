ee67cfd3a1ee802a9838f9f512de8cd4b6f429ce
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-06-21 00:13:35 +0300

    add register model

--- VideoPlayer/Models/RegisterViewModel.cs
+++ VideoPlayer/Models/RegisterViewModel.cs
@@ -2,9 +2,9 @@
 {
     public class RegisterViewModel
     {
-        public string? Email { get; set; }
-        public string? Name { get; set; }
-        public string? Password { get; set; }
-        public string? ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
