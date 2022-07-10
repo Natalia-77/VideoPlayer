9bd703dcb03fd2fe49280202c777f50fb5d8abc8
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-07-10 23:43:57 +0300

    add action,types for registration user

--- frontend/src/components/account/register/actions.ts
+++ frontend/src/components/account/register/actions.ts
@@ -12,13 +12,15 @@ import {
   export const RegisterNewUser=(data:IRegister)=> async(dispatch:Dispatch<RegisterAction>)=> {
 
     try{
+        dispatch({type:RegisterActionTypes.REGISTER_START});
         const response = await http.post("api/account/register",data);
         const token = response.data.token;
         dispatch({
-            type:RegisterActionTypes.REGISTER_START,
+            type:RegisterActionTypes.REGISTER_SUCCESS,
             payload:token
-            
-        })
+        });
+        
+        
       
     }
 
