7113df5a2a4732c9bc6efcbfc4f74933ea698578
vlasnata80@gmail.com <vlasnata80@gmail.com>
2022-07-12 23:37:47 +0300

    add action.

--- frontend/src/helpers/setUserInLocalStorage.ts
+++ frontend/src/helpers/setUserInLocalStorage.ts
@@ -5,11 +5,7 @@ import {RegisterAction} from '../components/account/register/types';
 export const setUserInLocalStorage=(token:string,dispatch:Dispatch<RegisterAction>)=>{
 
     setToken(token);
-    localStorage.setItem('user', token);    
+    localStorage.setItem('user', token);     
    
-    dispatch({
-        type: 
-        payload: 
-      });
 
 }
